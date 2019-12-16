1561cb497be4ff57d274e693e0b9e15a3f961336
서상원(Seo Sang won) <33479927+zhaoSeo@users.noreply.github.com>
2019-12-17 00:57:37 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Food({ name }) {
+  return <h1>i love { name }</h1>
+}
+
+const foodILike = [{
+  id:1,
+  name: "kimchi"
+},
+{
+  id:2,
+  name: "Samgyeopsal"  
+},
+{
+  id:3,
+  name: "bibimbab"
+}]
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} />)}
     </div>
   );
 }
