14156bf76f6b5b627bbcd99c613c9ae6089565ba
서상원(Seo Sang won) <33479927+zhaoSeo@users.noreply.github.com>
2019-12-17 11:58:41 +0900

    #3 2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
+//import PropTypes from "prop-types";
 
-function Food({ name }) {
-  return <h1>i love { name }</h1>
-}
-
-const foodILike = [{
-  id:1,
-  name: "kimchi"
-},
-{
-  id:2,
-  name: "Samgyeopsal"  
-},
-{
-  id:3,
-  name: "bibimbab"
-}]
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} />)}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}))
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}))
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
